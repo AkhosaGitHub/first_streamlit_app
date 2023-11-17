82ff6d5b6f7c2831ec7290e618ebe3abda593720
AkhosaGitHub <151162868+AkhosaGitHub@users.noreply.github.com>
2023-11-17 13:04:01 +0000

    Update streamlit_app.py
    Index displays Fruit Name Column as the Index instead of number

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,8 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # Importing pyhton libray pandas, importing csv data, displaying it on app
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Index displays fruit not number. 
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # New displaying data and using a widget 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))

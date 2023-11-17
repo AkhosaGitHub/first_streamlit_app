a38efd8c29ac4478b64947685a4874f1953363e9
AkhosaGitHub <151162868+AkhosaGitHub@users.noreply.github.com>
2023-11-17 12:41:03 +0000

    Update streamlit_app.py
    Created a widget for Create own fruit smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,23 @@
 import streamlit
 
+# Creating Title for App
 streamlit.title('My Parents New Healthy Diner')
 
+# Added in new section for Breakfast Menu and Items
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
+# Added in new section to build your own fruit smoothie
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+# Importing pyhton libray pandas, importing csv data, displaying it on app
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# New displaying data and using a widget 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
+
